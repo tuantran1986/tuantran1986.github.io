edfec5b61294eb9d9942054f1f78b92a93289f2f
tuantran1986 <61728265+tuantran1986@users.noreply.github.com>
2020-08-21 15:15:11 +0700

    THEM PROJECT 1 - TODO LIST

--- 1.js
+++ 1.js
@@ -8,33 +8,33 @@
     
     // CLICK - CUON XUONG :
     $('.mn1').click(function() {
-        console.log(vitrigioithieu);
+        // console.log(vitrigioithieu);
         $('html,body').animate({scrollTop: vitrigioithieu},800);
      });
 
     $('.mn2').click(function() {
-        console.log(vitrikynang);
+        // console.log(vitrikynang);
         $('html,body').animate({scrollTop: vitrikynang},800);
      });
 
     $('.mn3').click(function() {
-        console.log(vitrisanpham);
+        // console.log(vitrisanpham);
         $('html,body').animate({scrollTop: vitrisanpham},800);
      });
 
     $('.mn4').click(function() {
-        console.log(vitrilienhe);
+        // console.log(vitrilienhe);
         $('html,body').animate({scrollTop: vitrilienhe},800);
      });
 
     $('.mn5').click(function() {
-        console.log(vitricomment);
+        // console.log(vitricomment);
         $('html,body').animate({scrollTop: vitricomment},800);
      });
     
     // CLICK - LEN TOP :
     $('.mn0').click(function() {
-        console.log(vitrimenutren);
+        // console.log(vitrimenutren);
         $('html,body').animate({scrollTop: vitrimenutren},800);
      });
 
